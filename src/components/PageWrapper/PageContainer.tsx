a8570d84cc628a08e2be575d4f4b61e5dfb94932
Leopold <karabinioebalvrot@gmail.com>
2026-02-11 16:42:35 +0300

    fix: экспорт типов в PageWrapper

--- src/components/PageWrapper/PageContainer.tsx
+++
@@ -1,6 +0,0 @@
-import styles from "./PageWrapper.module.css";
-import { PageWrapperProps } from "./PageWrapper";
-
-export default function PageContainer({ children }: PageWrapperProps) {
-  return <div className={styles.pageContainer}>{children}</div>;
-}
