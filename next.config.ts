ad0b8c7d9c758204bec79153f8d86e6114cb92d8
Leopold <karabinioebalvrot@gmail.com>
2026-06-06 12:43:15 +0300

    Add remotePatterns for images from proxy domain

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,45 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   reactCompiler: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "api.dicebear.com",
+//         pathname: "/**",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "guicprnabbwmkpxhhrwg.supabase.co",
+//         pathname: "/storage/v1/object/public/**",
+//       },
+//     ],
+//   },
+
+//   async headers() {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         headers: [
+//           { key: "Access-Control-Allow-Origin", value: "*" },
+//           {
+//             key: "Access-Control-Allow-Methods",
+//             value: "GET,POST,PUT,DELETE,OPTIONS",
+//           },
+//           {
+//             key: "Access-Control-Allow-Headers",
+//             value: "Content-Type, Authorization",
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
+// для прокси
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -14,9 +56,13 @@ const nextConfig: NextConfig = {
         hostname: "guicprnabbwmkpxhhrwg.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
+      {
+        protocol: "https",
+        hostname: "api.xn--46-6kcay4al8ahci5n.xn--p1ai",
+        pathname: "/storage/v1/object/public/**",
+      },
     ],
   },
-  //
   async headers() {
     return [
       {
@@ -35,7 +81,6 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  //
 };
 
 export default nextConfig;
