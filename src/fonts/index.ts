bd352b38f1a5de36ca382d4b44f1fa59ea693c98
Leopold <karabinioebalvrot@gmail.com>
2026-06-19 09:10:43 +0300

    delete not use fonts

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -1,43 +1,3 @@
-// import {
-//   Geist,
-//   Geist_Mono,
-//   IBM_Plex_Sans,
-//   Nunito,
-//   Space_Grotesk,
-// } from "next/font/google";
-
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin", "cyrillic"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin", "cyrillic"],
-// });
-
-// const nunito = Nunito({
-//   variable: "--font-nunito",
-//   subsets: ["latin", "cyrillic"],
-//   weight: ["200", "400", "600", "700"],
-//   display: "swap",
-// });
-
-// const spaceGrotesk = Space_Grotesk({
-//   variable: "--font-space-grotesk",
-//   subsets: ["latin"], // Обратите внимание: в Google Fonts для него обычно нет кириллицы
-//   weight: ["300", "400", "500", "600", "700"],
-//   display: "swap",
-// });
-
-// const ibmPlex = IBM_Plex_Sans({
-//   variable: "--font-ibm-plex",
-//   subsets: ["latin", "cyrillic"],
-//   weight: ["300", "400", "500", "600"],
-// });
-
-// export const fontVariables = `${nunito.variable} ${geistSans.variable} ${geistMono.variable} ${spaceGrotesk.variable} ${ibmPlex.variable}`;
-
 import localFont from "next/font/local";
 
 export const geistSans = localFont({
@@ -104,104 +64,104 @@ export const geistMono = localFont({
   ],
 });
 
-export const ibmPlex = localFont({
-  variable: "--font-ibm-plex",
-  display: "swap",
-  src: [
-    {
-      path: "./ibm/ibm-plex-sans-v23-cyrillic_latin-300.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "./ibm/ibm-plex-sans-v23-cyrillic_latin-300italic.woff2",
-      weight: "300",
-      style: "italic",
-    },
-    {
-      path: "./ibm/ibm-plex-sans-v23-cyrillic_latin-500.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "./ibm/ibm-plex-sans-v23-cyrillic_latin-500italic.woff2",
-      weight: "500",
-      style: "italic",
-    },
-    {
-      path: "./ibm/ibm-plex-sans-v23-cyrillic_latin-600.woff2",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "./ibm/ibm-plex-sans-v23-cyrillic_latin-600italic.woff2",
-      weight: "600",
-      style: "italic",
-    },
-    {
-      path: "./ibm/ibm-plex-sans-v23-cyrillic_latin-700.woff2",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "./ibm/ibm-plex-sans-v23-cyrillic_latin-italic.woff2",
-      weight: "400",
-      style: "italic",
-    },
-    {
-      path: "./ibm/ibm-plex-sans-v23-cyrillic_latin-regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-  ],
-});
+// export const ibmPlex = localFont({
+//   variable: "--font-ibm-plex",
+//   display: "swap",
+//   src: [
+//     {
+//       path: "./ibm/ibm-plex-sans-v23-cyrillic_latin-300.woff2",
+//       weight: "300",
+//       style: "normal",
+//     },
+//     {
+//       path: "./ibm/ibm-plex-sans-v23-cyrillic_latin-300italic.woff2",
+//       weight: "300",
+//       style: "italic",
+//     },
+//     {
+//       path: "./ibm/ibm-plex-sans-v23-cyrillic_latin-500.woff2",
+//       weight: "500",
+//       style: "normal",
+//     },
+//     {
+//       path: "./ibm/ibm-plex-sans-v23-cyrillic_latin-500italic.woff2",
+//       weight: "500",
+//       style: "italic",
+//     },
+//     {
+//       path: "./ibm/ibm-plex-sans-v23-cyrillic_latin-600.woff2",
+//       weight: "600",
+//       style: "normal",
+//     },
+//     {
+//       path: "./ibm/ibm-plex-sans-v23-cyrillic_latin-600italic.woff2",
+//       weight: "600",
+//       style: "italic",
+//     },
+//     {
+//       path: "./ibm/ibm-plex-sans-v23-cyrillic_latin-700.woff2",
+//       weight: "700",
+//       style: "normal",
+//     },
+//     {
+//       path: "./ibm/ibm-plex-sans-v23-cyrillic_latin-italic.woff2",
+//       weight: "400",
+//       style: "italic",
+//     },
+//     {
+//       path: "./ibm/ibm-plex-sans-v23-cyrillic_latin-regular.woff2",
+//       weight: "400",
+//       style: "normal",
+//     },
+//   ],
+// });
 
-export const nunito = localFont({
-  variable: "--font-nunito",
-  display: "swap",
-  src: [
-    {
-      path: "./nunito/nunito-v32-cyrillic_latin-500.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "./nunito/nunito-v32-cyrillic_latin-500italic.woff2",
-      weight: "500",
-      style: "italic",
-    },
-    {
-      path: "./nunito/nunito-v32-cyrillic_latin-600.woff2",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "./nunito/nunito-v32-cyrillic_latin-600italic.woff2",
-      weight: "600",
-      style: "italic",
-    },
-    {
-      path: "./nunito/nunito-v32-cyrillic_latin-700.woff2",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "./nunito/nunito-v32-cyrillic_latin-700italic.woff2",
-      weight: "700",
-      style: "italic",
-    },
-    {
-      path: "./nunito/nunito-v32-cyrillic_latin-italic.woff2",
-      weight: "400",
-      style: "italic",
-    },
-    {
-      path: "./nunito/nunito-v32-cyrillic_latin-regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-  ],
-});
+// export const nunito = localFont({
+//   variable: "--font-nunito",
+//   display: "swap",
+//   src: [
+//     {
+//       path: "./nunito/nunito-v32-cyrillic_latin-500.woff2",
+//       weight: "500",
+//       style: "normal",
+//     },
+//     {
+//       path: "./nunito/nunito-v32-cyrillic_latin-500italic.woff2",
+//       weight: "500",
+//       style: "italic",
+//     },
+//     {
+//       path: "./nunito/nunito-v32-cyrillic_latin-600.woff2",
+//       weight: "600",
+//       style: "normal",
+//     },
+//     {
+//       path: "./nunito/nunito-v32-cyrillic_latin-600italic.woff2",
+//       weight: "600",
+//       style: "italic",
+//     },
+//     {
+//       path: "./nunito/nunito-v32-cyrillic_latin-700.woff2",
+//       weight: "700",
+//       style: "normal",
+//     },
+//     {
+//       path: "./nunito/nunito-v32-cyrillic_latin-700italic.woff2",
+//       weight: "700",
+//       style: "italic",
+//     },
+//     {
+//       path: "./nunito/nunito-v32-cyrillic_latin-italic.woff2",
+//       weight: "400",
+//       style: "italic",
+//     },
+//     {
+//       path: "./nunito/nunito-v32-cyrillic_latin-regular.woff2",
+//       weight: "400",
+//       style: "normal",
+//     },
+//   ],
+// });
 
 export const spaceGrotesk = localFont({
   variable: "--font-space-grotesk",
@@ -238,7 +198,6 @@ export const spaceGrotesk = localFont({
 export const fontVariables = `
   ${geistSans.variable}
   ${geistMono.variable}
-  ${nunito.variable}
   ${spaceGrotesk.variable}
-  ${ibmPlex.variable}
+  
 `;
